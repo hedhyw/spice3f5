2a29e2b43d6f214fa927385cc7725ef5faaf57a3
hedhyw <hedhyw@yahoo.com>
2018-05-01 09:00:24 +0300

    Remove ignored files

--- src/bin/tunepc.c
+++
@@ -1,9 +0,0 @@
-char	*Spice_Exec_Dir	= "C:\\spice3f5";
-char	*Spice_Lib_Dir	= "C:\\spice3f5";
-char	Spice_OptChar	= '-';
-char	*Def_Editor	= "notepad.exe";
-int	AsciiRawFile	= 0;
-
-char	*Bug_Addr	= "123@123.ru";
-char	*Spice_Host	= "127.0.0.1";
-char	*Spiced_Log	= "C:\\spice3f5.log";
